50ed03fe38bc613a263ffcf9b05e7dd35b6f7818
Kaelin <ka3lin@gmail.com>
2020-03-18 11:44:21 -0700

    pushing build folder

---
+++ client/build/precache-manifest.81ae548979da5f2df2b6b10fffc0b4c8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "273e4bb7129b417e0b20ccdad4890f85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "874aefc4cea2f629c403",
+    "url": "/static/css/main.d642f6a9.chunk.css"
+  },
+  {
+    "revision": "845f35080b4caae7bfe5",
+    "url": "/static/js/2.8608b76c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.8608b76c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "874aefc4cea2f629c403",
+    "url": "/static/js/main.a4b57e69.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "1a2a157bfd94b250884c09f1bd57a0bb",
+    "url": "/static/media/SourceSansPro-Bold.1a2a157b.otf"
+  },
+  {
+    "revision": "5e1d162e634a102d7da8db597825a764",
+    "url": "/static/media/SourceSansPro-Regular.5e1d162e.otf"
+  },
+  {
+    "revision": "4785ffc8f434dd14867f8326ec680ede",
+    "url": "/static/media/down-arrow.4785ffc8.svg"
+  },
+  {
+    "revision": "6385bc87d79439d6076d937c0cea8e1f",
+    "url": "/static/media/keyboard.6385bc87.svg"
+  }
+]);
